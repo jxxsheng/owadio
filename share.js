3ab697bdc04b0adfd23709498ded273a17da9f19
jxxsheng <131389293+jxxsheng@users.noreply.github.com>
2023-04-24 01:45:55 +0800

    Add files via upload

--- share.js
+++ share.js
@@ -7,25 +7,26 @@ function share() {
 
   let days = globalVar1;
   let a = globalVar2;
-
-  let postUrl = encodeURI(document.location.href);  
+  let postUrl = encodeURI(document.location.href);
   let postTitle;
 
   if (days <= 0){
-    postTitle = encodeURI(" WHERE GOT TIME? OWADIO!\n Count your days now!\n");
+    postTitle = encodeURI("WHERE GOT TIME? OWADIO!\n");
   }
   else if (days == 1){
-    postTitle = encodeURI(" 1 MORE DAY TO ORD!\n Count your days now!\n")
+    postTitle = encodeURI("1 MORE DAY TO ORD!\n")
   }
   else if ((days - a) > 1){
-    postTitle = encodeURI(" " + (days - a) + " more days to Enlistment!\n Count your days now!\n");
+    postTitle = encodeURI((days - a) + " more days to Enlistment!\n");
   }
   else if ((days - a) == 1){
-    postTitle = encodeURI(" " + (days - a) + " more day to Enlistment!\n Count your days now!\n");
+    postTitle = encodeURI((days - a) + " more day to Enlistment!\n");
   }
   else {
-    postTitle = encodeURI(" " + days + " Days to ORD!\n Count your days now!\n");
+    postTitle = encodeURI(days + " days to ORD!\n");
   }
+
+  let postTwitter = postTitle.replace("%0A"," ");
 
   facebook.setAttribute(
     "href",
@@ -34,12 +35,11 @@ function share() {
 
   twitter.setAttribute(
     "href",
-    `https://twitter.com/share?url=${postUrl}&text=${postTitle}`
-  );
-
+    `https://twitter.com/intent/tweet?text=${postTwitter}&url=${postUrl}`
+  )
   whatsapp.setAttribute(
     "href",
-    `https://wa.me/?text=${postTitle} ${postUrl}`
+    `https://wa.me/?text=${postTitle}${postUrl}`
   );
 
   telegram.setAttribute(
